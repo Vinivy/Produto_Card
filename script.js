4a512a9691de7ac1d18ec43f4fbf4f0e47d0c616
Vinivy <vinvygabriel8@gmail.com>
2023-05-25 01:41:00 -0300

    Post

---
+++ script.js
@@ -0,0 +1,8 @@
+const Area = document.getElementById('picture')
+let Trocas = document.querySelectorAll('.imgs')
+let Icon = document.querySelectorAll('.icon')
+
+function Rotacion() {
+  imgs.scr('scr', './Images/Sofá-360.gif')
+}
+//Icon.src('scr', './Images/icon-close.svg')
