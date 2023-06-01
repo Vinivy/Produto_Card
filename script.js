2cafc48e1e3a9abec2dfbfca38a334a158b0a570
Vinivy <vinvygabriel8@gmail.com>
2023-05-31 22:11:51 -0300

    Product Card

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-const Area = document.getElementById('picture')
-let Trocas = document.querySelectorAll('.imgs')
-let Icon = document.querySelectorAll('.icon')
+//butão de função para voltar a img e icon normal
+const buttonclose = document.querySelector('.buttonX')
+// imgsofá e gifsofá
+const Trocadeimg = document.querySelector('.imgs')
 
-function Rotacion() {
-  imgs.scr('scr', './Images/Sofá-360.gif')
+//botão dessa função
+let buttonO = document.querySelector('.rodate')
+//função de gif e de botão X
+function Rotacion(troca) {
+  //de img pra gif
+  Trocadeimg.setAttribute('src', './Images/Sofá-360.gif')
+  //mostrar outro botão e nova função
+  buttonclose.style.display = 'inline'
+  //essa funtion vai sumir e vai ser esse looping
+  buttonO.style.display = 'none'
 }
-//Icon.src('scr', './Images/icon-close.svg')
+
+function Backimg() {
+  //de volta img
+  Trocadeimg.setAttribute('src', './Images/Sofá.png')
+  //de volta ao function gif
+  buttonO.style.display = 'inline'
+
+  buttonclose.style.display = 'none'
+}
+
+function carrinho() {
+  alert('Feito!')
+}
